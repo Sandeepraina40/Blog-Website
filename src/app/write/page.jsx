63174255d10b6fdc9b080d0e2791273239b6043b
Sandeep Raina <127074583+Sandeepraina40@users.noreply.github.com>
2025-04-05 18:23:15 +0530

    Update page.jsx

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -80,7 +80,7 @@ const WritePage = () => {
       .replace(/^-+|-+$/g, "");
 
   const handleSubmit = async () => {
-    const res = await fetch(`${process.env.NEXT_URL}/api/posts`, {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/posts`, {
       method: "POST",
       body: JSON.stringify({
         title,
